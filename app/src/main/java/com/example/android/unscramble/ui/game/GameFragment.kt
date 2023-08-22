aed9ab95a0bedf42fd45fe26a3ce4ece9ec3a11d
Glenn Halls <gjhalls@gmail.com>
2023-08-22 10:09:57 +1000

    Add: data binding variables

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -55,10 +55,13 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.gameViewModel = viewModel
+        binding.maxNoOfWords = MAX_NO_OF_WORDS
 
         // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
+        binding.lifecycleOwner = viewLifecycleOwner
         // Update the UI
         viewModel.score.observe(viewLifecycleOwner) {
             binding.score.text = getString(R.string.score, it)
